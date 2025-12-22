7be6e92be7ba20dbaee7389079b93d9e009a48c3
MunjN <134438716+MunjN@users.noreply.github.com>
2025-12-22 14:30:12 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "maplibre-gl/dist/maplibre-gl.css";
 
+// Placeholder wrapper for upcoming global providers (Auth, Bookmarks, etc.).
+// We'll replace the body once we add those modules.
+function AppProviders({ children }) {
+  return children;
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AppProviders>
+      <App />
+    </AppProviders>
   </React.StrictMode>
 );
