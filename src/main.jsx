956230ebe9c591348bfdd686d0a9611ecc1ba870
MunjN <134438716+MunjN@users.noreply.github.com>
2025-12-22 15:38:09 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "./index.css";
 import "maplibre-gl/dist/maplibre-gl.css";
 
 import { AuthProvider } from "./auth/AuthContext.jsx";
